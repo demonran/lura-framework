e1d22973f8021269b18e2a56b0dc7ad865d0ce4a
ran.liu1 <ran.liu1@thoughtworks.com>
2021-12-08 10:52:31 +0800

    rename root annotation use @LuraBootApplication

--- lura-framework-spring-boot-starter/src/main/java/me/luraframework/boot/LuraBootApplication.java
+++ lura-framework-spring-boot-starter/src/main/java/me/luraframework/boot/LuraBootApplication.java
@@ -4,13 +4,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Inherited
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @EnableDiscoveryClient
 @SpringBootApplication
-public @interface LuraFramework {
+public @interface LuraBootApplication {
 }
