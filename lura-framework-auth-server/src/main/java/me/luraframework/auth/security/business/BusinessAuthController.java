d48225764e30d96ad5798de1e63ded00bdb939bf
ran.liu1 <ran.liu1@thoughtworks.com>
2021-12-30 18:54:51 +0800

    auth server set servlet content /auth

--- lura-framework-auth-server/src/main/java/me/luraframework/auth/security/business/BusinessAuthController.java
+++ lura-framework-auth-server/src/main/java/me/luraframework/auth/security/business/BusinessAuthController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
-@RequestMapping("business/auth")
+@RequestMapping("business")
 @RequiredArgsConstructor
 public class BusinessAuthController {
 
--- lura-framework-auth-server/src/main/java/me/luraframework/auth/security/customer/CustomerAuthController.java
+++ lura-framework-auth-server/src/main/java/me/luraframework/auth/security/customer/CustomerAuthController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
-@RequestMapping("auth")
+@RequestMapping("customer")
 @RequiredArgsConstructor
 public class CustomerAuthController {
 
